be60812aa3b2b562d8bbf048ead62bc3aca2e69d
ssamt <73675935+ssamt@users.noreply.github.com>
2021-11-15 14:01:46 +0900

    javascript abstracted over levels

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-function change_rate(a, b) {
-    return 1/(Math.E-a/b)
-}
-
 function progress(x, goal) {
     if(-goal <= x && x <= goal) {
         return x/goal
@@ -24,69 +20,6 @@ function hide_alert() {
     document.getElementById("alert").innerText = ""
 }
 
-function set_var() {
-    temp_a = Number(document.getElementById("a_input").value);
-    temp_b = Number(document.getElementById("b_input").value);
-    if(temp_a < 0) {
-        show_alert("a cannot be smaller than 0!")
-    } else if(temp_b < 1) {
-        show_alert("b cannot be smaller than 1!")
-    } else if(Number.isInteger(temp_a) && Number.isInteger(temp_b)) {
-        if(temp_a+temp_b <= gold[stage]) {
-            a = temp_a
-            b = temp_b
-            hide_alert()
-            calculate()
-        } else {
-            show_alert("You do not have enough gold!")
-        }
-    } else {
-        show_alert("Variables should be integers!")
-    }
-}
-
-function next_stage() {
-    stage++
-    document.getElementById("next_stage").className = "btn disabled"
-    x = 0
-    document.getElementById("x").innerHTML = cut_decimal(x, 2)
-    calculate()
-}
-
-function calculate() {
-    dx = change_rate(a, b)
-    document.getElementById("a").innerHTML = a
-    document.getElementById("b").innerHTML = b
-    document.getElementById("dx").innerHTML = cut_decimal(dx, 2)
-    document.getElementById("e_val").innerHTML = Math.E
-    document.getElementById("a_div_b").innerHTML = a/b
-    document.getElementById("end").innerHTML = goal[stage]
-    document.getElementById("gold").innerHTML = gold[stage]
-}
-
-function step() {
-    x = x+dx*tick
-    bar = progress(x, goal[stage])*100
-    if(bar >= 100) {
-        bar = 100
-        document.getElementById("next_stage").className = "waves-effect waves-light btn"
-    }
-    if(bar <= -100) {
-        bar = -100
-    }
-    document.getElementById("progress").innerHTML = cut_decimal(bar, 2)
-    document.getElementById("x").innerHTML = cut_decimal(x, 2)
-    if(bar >= 0) {
-        document.getElementById("bar").style.width = bar+"%"
-        document.getElementById("bar_bg").style.backgroundColor = "darkblue"
-        document.getElementById("bar").style.backgroundColor = "steelblue"
-    } else {
-        document.getElementById("bar").style.width = (100+bar)+"%"
-        document.getElementById("bar_bg").style.backgroundColor = "red"
-        document.getElementById("bar").style.backgroundColor = "darkblue"
-    }
-}
-
 document.getElementById("inputs").addEventListener("keyup", enter)
 
 function enter(event) {
@@ -95,14 +28,121 @@ function enter(event) {
     }
 }
 
-let a = 0
-let b = 1
-let dx
-let x = 1
-let bar = 0
-let tick = 1
-let stage = 0
-let gold = [10, 100, 10000]
-let goal = [150, 15000, 5e+8]
-calculate()
-let main_loop = setInterval(step, tick*1000)
+class Level {
+    constructor() {
+        this.dx = 0
+        this.x = 1
+        this.bar = 0
+        this.tick = 1
+        this.stage = 0
+    }
+
+    step() {
+        this.x = this.x+this.dx*this.tick
+        this.bar = progress(this.x, this.goal[this.stage])*100
+        if(this.bar >= 100) {
+            this.bar = 100
+            document.getElementById("next_stage").className = "waves-effect waves-light btn"
+        }
+        if(this.bar <= -100) {
+            this.bar = -100
+        }
+        document.getElementById("progress").innerHTML = cut_decimal(this.bar, 2)
+        document.getElementById("x").innerHTML = cut_decimal(this.x, 2)
+        if(this.bar >= 0) {
+            document.getElementById("bar").style.width = this.bar+"%"
+            document.getElementById("bar_bg").style.backgroundColor = "darkblue"
+            document.getElementById("bar").style.backgroundColor = "steelblue"
+        } else {
+            document.getElementById("bar").style.width = (100+this.bar)+"%"
+            document.getElementById("bar_bg").style.backgroundColor = "red"
+            document.getElementById("bar").style.backgroundColor = "darkblue"
+        }
+    }
+
+    run() {
+        this.calculate()
+        this.main_loop = setInterval(this.step.bind(this), this.tick*1000)
+    }
+
+    end() {
+        clearInterval(this.main_loop)
+    }
+}
+
+class Level1 extends Level {
+    constructor() {
+        super();
+        this.a = 0
+        this.b = 1
+        this.gold = [10, 100, 10000]
+        this.goal = [150, 15000, 5e+8]
+        this.max_stage = this.goal.length
+    }
+
+    change_rate(a, b) {
+        return 1/(Math.E-a/b)
+    }
+
+    set_var() {
+        let temp_a = Number(document.getElementById("a_input").value);
+        let temp_b = Number(document.getElementById("b_input").value);
+        if(temp_a < 0) {
+            show_alert("a cannot be smaller than 0!")
+        } else if(temp_b < 1) {
+            show_alert("b cannot be smaller than 1!")
+        } else if(Number.isInteger(temp_a) && Number.isInteger(temp_b)) {
+            if(temp_a+temp_b <= this.gold[this.stage]) {
+                this.a = temp_a
+                this.b = temp_b
+                hide_alert()
+                this.calculate()
+            } else {
+                show_alert("You do not have enough gold!")
+            }
+        } else {
+            show_alert("Variables should be integers!")
+        }
+    }
+
+    next_stage() {
+        this.stage++
+        document.getElementById("next_stage").className = "btn disabled"
+        this.x = 0
+        document.getElementById("x").innerHTML = cut_decimal(this.x, 2)
+        this.calculate()
+    }
+
+    calculate() {
+        this.dx = this.change_rate(this.a, this.b)
+        document.getElementById("a").innerHTML = this.a
+        document.getElementById("b").innerHTML = this.b
+        document.getElementById("dx").innerHTML = cut_decimal(this.dx, 2)
+        document.getElementById("e_val").innerHTML = Math.E
+        document.getElementById("a_div_b").innerHTML = this.a/this.b
+        document.getElementById("end").innerHTML = this.goal[this.stage]
+        document.getElementById("gold").innerHTML = this.gold[this.stage]
+    }
+}
+
+level = 0
+levels = [new Level1()]
+levels[level].run()
+
+function set_var() {
+    levels[level].set_var()
+}
+
+function next_stage() {
+    if(levels[level].stage === levels[level].max_stage-1) {
+        levels[level].end()
+        if(level === levels.length-1) {
+            show_alert("End")
+        } else {
+            level++
+            levels[level].run()
+        }
+    } else {
+        levels[level].next_stage()
+    }
+}
